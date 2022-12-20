36c76ce05c153d06dee453afe1fe414410cd0d75
Mohima2 <89738019+Mohima2@users.noreply.github.com>
2022-12-20 21:59:19 +0600

    Update emp_insert.php

--- emp_insert.php
+++ emp_insert.php
@@ -3,24 +3,25 @@
 
 if(isset($_POST["empID"]))
 {
- $connect = new PDO("mysql:host=localhost;dbname=jbl2", "root", "");
+ $connect = new PDO("mysql:host=localhost;dbname=project", "root", "");
  $serial = uniqid();
  for($count = 0; $count < count($_POST["empID"]); $count++)
  {  
-  $query = "INSERT INTO employee
-  (serial, empID, name(bn), designation, cell_ID, usertype, password) 
-  VALUES (:serial, :empID, :name(bn), :designation, :cell_ID, :usertype, :password)
+  $query = "INSERT INTO employee 
+  (order_id, empID, name(bn), name(en), designation, cell_ID, usertype, password) 
+  VALUES (:order_id, :empID, :name(bn), :name(en), :designation, :cell_ID, :usertype, :password)
   ";
   $statement = $connect->prepare($query);
   $statement->execute(
    array(
     ':serial'   => $serial,
-    ':empID'  => $_POST["empID"][$count], 
+    ':empID	'  => $_POST["empID"][$count], 
     ':name(bn)' => $_POST["name(bn)"][$count], 
-    ':designation'  => $_POST["designation"][$count],
-    ':cell_ID'  => $_POST["cell_ID"][$count],
-    ':usertype'  => $_POST["usertype"][$count],
-    ':password'  => $_POST["password"][$count]
+    ':name(en)'  => $_POST["name(en)"][$count],
+    ':designation' => $_POST["designation"][$count],
+    ':cell_ID' => $_POST["cell_ID"][$count],
+    ':usertype' => $_POST["usertype"][$count],
+    ':password' => $_POST["password"][$count]
    )
   );
  }
