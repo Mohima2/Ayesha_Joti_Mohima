d8ee3bdde9edf477d0726354b30527f03c499fd2
Mohima2 <89738019+Mohima2@users.noreply.github.com>
2022-12-20 21:58:41 +0600

    Update employee.php

--- employee.php
+++ employee.php
@@ -1,10 +1,12 @@
 <?php
 
-$connect = new PDO("mysql:host=localhost;dbname=jbl2", "root", "");
+
+
+$connect = new PDO("mysql:host=localhost;dbname=project", "root", "");
 function fill_unit_select_box($connect)
 { 
  $output = '';
- $query = "SELECT * FROM desination ORDER BY id ASC";
+ $query = "SELECT * FROM designation";
  $statement = $connect->prepare($query);
  $statement->execute();
  $result = $statement->fetchAll();
@@ -17,213 +19,89 @@ function fill_unit_select_box($connect)
 
 function fill_unit_select_box2($connect)
 { 
- $output2 = '';
- $query = "SELECT * FROM cell ORDER BY cell_code ASC";
+ $output = '';
+ $query = "SELECT * FROM cell";
  $statement = $connect->prepare($query);
  $statement->execute();
  $result = $statement->fetchAll();
  foreach($result as $row)
  {
-  $output2 .= '<option value="'.$row["cell_title"].'">'.$row["cell_title"].'</option>';
+  $output .= '<option value="'.$row["cell_code"].'">'.$row["cell_code"].'</option>';
  }
- return $output2;
+ return $output;
 }
 
-function fill_unit_select_box3($connect)
-{ 
- $output3 = '';
- $query = "SELECT * FROM user ORDER BY roleID ASC";
- $statement = $connect->prepare($query);
- $statement->execute();
- $result = $statement->fetchAll();
- foreach($result as $row)
- {
-  $output3 .= '<option value="'.$row["usertype"].'">'.$row["usertype"].'</option>';
- }
- return $output3;
-}
-?>
 
-
-<?php 
-
-$conn = mysqli_connect("localhost", "root", "", "jbl2");
 
 ?>
-
-
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Employee</title>
-</head>
-<body>
-<?php include ("header.php"); ?>
-
-<div class="container">
+<html>
+ <head>
+  <title>Add Remove Select Box Fields Dynamically using jQuery Ajax in PHP</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.min.map">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.2/jquery.slim.min.map"></script>
+ </head>
+ <body>
+  <br />
+  <div class="container">
+   <h4 align="center">Add Employee in the List</h4>
+   <br />
    <form method="post" id="insert_form">
     <div class="table-repsonsive">
      <span id="error"></span>
      <table class="table table-bordered" id="item_table">
       <tr>
-       <th>Employee ID</th>
-       <th>Name(bn)</th>
-       <th>Name(en)</th>
+       <th>Employee Code</th>
+       <th>Name(Bangla)</th>
+       <th>Name(English)</th>
        <th>Designation</th>
-       <th>Cell </th>
+       <th>Cell Code</th>
        <th>User Role</th>
        <th>Password</th>
       </tr>
-      <tr>
-  <td><input type="text" name="empID[]" class="form-control emp_ID" /></td>
-  <td><input type="text" name="name(bn)[]" class="form-control name(bn)" /></td>
-  <td><input type="text" name="name(en)[]" class="form-control name(en)" /></td>
-
-  <td><select name="designation[]" class="form-control selectpicker" ><option value=""></option><?php echo fill_unit_select_box($connect); ?></select></td>
-
-  <td><select name="cell_ID[]" class="form-control selectpicker"><option value=""></option><?php echo fill_unit_select_box2($connect); ?></select></td>
-  <td><select name="usertype[]" class="form-control selectpicker"><option value=""></option><?php echo fill_unit_select_box3($connect); ?></select></td>
-  <td><input type="password" name="password[]" class="form-control password" placeholder="123456" /></td>
-  </tr>
- </table>
-
-     <div><button type="button" name="add" class="btn btn-success btn-sm add"><span class="glyphicon glyphicon-plus"></span></button></div>
+     </table>
+     <button type="button" name="add" class="btn btn-success btn-sm add"><span class="glyphicon glyphicon-plus"></span></button>
+     <div align="center">
+      <input type="submit" name="submit" class="btn btn-info" value="Insert" />
+     </div>
     </div>
    </form>
   </div>
+ </body>
+</html>
 
-  <div align="center">
-      <input type="submit" name="submit" class="btn btn-info" value="Add Employee" />
-     </div>
-
-
-<div class="dashboard flex-grow-1">
-
-        <div class="container-fluid px-4">
-        <div class="row at-4">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h1>
-                           Employee Details
-                           
-                        </h1>
-                    </div>
-
-                <div class="card-body">
-        
-                <div class="table-responsive">
-            
-                <table class="table table-bordered table-striped">
-                
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Employee Name</th>
-                        <th>Designation</th>
-                        <th>Code</th>
-                        <th>Cell</th>
-                        <th>Edit Info</th>
-
-                    </tr>
-                   
-                </thead>
-                <tbody>
-                    <?php 
-                         $check = "SELECT * FROM employee";
-                         $posts_run = mysqli_query($conn, $check);
-
-                         if(mysqli_num_rows($posts_run) >0)
-                         {
-                            foreach($posts_run as $post)
-                            {
-                                ?>
-                                <tr>
-                                    <td><?= $post['serial'] ?></td>
-                                    <td><?= $post['name(bn)'] ?></td>
-                                    <td><?= $post['designation'] ?></td>
-                                    <td><?= $post['empID'] ?></td>
-                                    <td> <?= $post['cell_ID'] ?></td>
-                                 
-                                    <td>
-                                        <a href="#" class="btn btn-success">Update</a>
-                                    </td>
-                                    
-                                </tr>
-
-                                <?php
-
-                            }
-                         }
-                         else
-                         {
-                    ?>
-                    <tr>
-                        <td colspan="6">No Record Found!</td>
-                    </tr>
-                    <?php 
-                     }
-                    ?>
-                    <tr>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    </div>
- </div>
-</div>
-</div></div></div>
-
-
-
-  <script type="text/javascript">
+<script>
 $(document).ready(function(){
+ 
  $(document).on('click', '.add', function(){
   var html = '';
   html += '<tr>';
   html += '<td><input type="text" name="empID[]" class="form-control empID" /></td>';
   html += '<td><input type="text" name="name(bn)[]" class="form-control name(bn)" /></td>';
   html += '<td><input type="text" name="name(en)[]" class="form-control name(en)" /></td>';
-  html += '<td><select name="designation[]" class="form-control selectpicker"><option value=""></option><?php echo fill_unit_select_box($connect); ?></select></td>';
-  html += '<td><select name="cell_ID[]" class="form-control selectpicker"><option value=""></option><?php echo fill_unit_select_box2($connect); ?></select></td>';
-  html += '<td><select name="usertype[]" class="form-control selectpicker"><option value=""></option><?php echo fill_unit_select_box3($connect); ?></select></td>';
-  html += '<td><input type="password" name="password[]" class="form-control password"/></td></tr>';
-  
+  html += '<td><select name="designation[]" class="form-control designation"><option value="">Select Unit</option><?php echo fill_unit_select_box($connect); ?></select></td>';
+  html += '<td><select name="cell_ID[]" class="form-control cell_ID"><option value="">Select Unit</option><?php echo fill_unit_select_box2($connect); ?></select></td>';
+  html += '<td><select name="usertype[]" class="form-control usertype"><option value="">Select Unit</option><?php echo fill_unit_select_box($connect); ?></select></td>';
+  html += '<td><input type="password" name="password[]" class="form-control password" /></td>';
+  html += '<td><button type="button" name="remove" class="btn btn-danger btn-sm remove"><span class="glyphicon glyphicon-minus"></span></button></td></tr>';
   $('#item_table').append(html);
  });
-
-
  
- $(document).on('click', '.remove', function(){  
+ $(document).on('click', '.remove', function(){
   $(this).closest('tr').remove();
  });
-
  
- 
-  /*('.cell_ID').each(function(){
-   var count = 1;
-   if($(this).val() == '')
-   {
-    error += "<p>Select Cell at "+count+" Row</p>";
-    return false;
-   }
-   count = count + 1;
-  });*/
-
-  $('#insert_form').on('submit', function(event){
+ $('#insert_form').on('submit', function(event){
   event.preventDefault();
   var error = '';
-  $('.name(en)').each(function(){
+  $('.empID').each(function(){
    var count = 1;
    if($(this).val() == '')
    {
-    error += "<p>Enter Name at "+count+" Row</p>";
+    error += "<p>Enter Employee Code at "+count+" Row</p>";
     return false;
    }
    count = count + 1;
@@ -233,28 +111,26 @@ $(document).ready(function(){
    var count = 1;
    if($(this).val() == '')
    {
-    error += "<p>Enter designation at "+count+" Row</p>";
+    error += "<p>Enter Designation at "+count+" Row</p>";
     return false;
    }
    count = count + 1;
   });
   
-  $('.usertype').each(function(){
+  $('.password').each(function(){
    var count = 1;
    if($(this).val() == '')
    {
-    error += "<p>Select Usertype at "+count+" Row</p>";
+    error += "<p>Set Password at "+count+" Row</p>";
     return false;
    }
    count = count + 1;
   });
-
-  console.log('stated document');
   var form_data = $(this).serialize();
   if(error == '')
   {
    $.ajax({
-    url:"insertEmp.php",
+    url:"insert.php",
     method:"POST",
     data:form_data,
     success:function(data)
@@ -275,15 +151,3 @@ $(document).ready(function(){
  
 });
 </script>
-
-  
-  
-
-
-
-<script src="https://kit.fontawesome.com/605c1f1072.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" ></script>
-
-
-</body>
-</html>
